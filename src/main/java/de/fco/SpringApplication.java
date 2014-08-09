026a2acbcad046a7a7e336790f15cf96e8b1961e
burrob <newmeadows@gmx.net>
2014-08-09 12:52:36 +0200

    Replaced the deprecated persistence unit with the current one.

--- src/main/java/de/fco/SpringApplication.java
+++ src/main/java/de/fco/SpringApplication.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *
  */
 package de.fco;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -30,46 +31,47 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement
 public class SpringApplication {
 
-	// @Bean
-	// public BasicDataSource dataSource() throws Exception {
-	// final Properties properties = new Properties();
-	// properties.setProperty("driverClassName", "org.hsqldb.jdbcDriver");
-	// properties.setProperty("url", "jdbc\\:hsqldb\\:mem\\:fco-tm");
-	// properties.setProperty("username", "");
-	// properties.setProperty("password", "");
-	// return BasicDataSourceFactory.createDataSource(properties);
-	// }
+    // @Bean
+    // public BasicDataSource dataSource() throws Exception {
+    // final Properties properties = new Properties();
+    // properties.setProperty("driverClassName", "org.hsqldb.jdbcDriver");
+    // properties.setProperty("url", "jdbc\\:hsqldb\\:mem\\:fco-tm");
+    // properties.setProperty("username", "");
+    // properties.setProperty("password", "");
+    // return BasicDataSourceFactory.createDataSource(properties);
+    // }
 
-	@Bean
-	public DataSource dataSource() {
-		final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		return builder.setType(EmbeddedDatabaseType.HSQL).build();
-	}
+    @Bean
+    public DataSource dataSource() {
+        final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+    }
 
-	@Bean
-	public EntityManagerFactory entityManagerFactory() {
-		final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		vendorAdapter.setGenerateDdl(true);
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
 
-		final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		factory.setJpaVendorAdapter(vendorAdapter);
-		factory.setPackagesToScan("de.fco.domain");
-		factory.setDataSource(dataSource());
-		factory.afterPropertiesSet();
+        final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("de.fco.domain");
+        factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
 
-		return factory.getObject();
-	}
+        return factory.getObject();
+    }
 
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-		final JpaTransactionManager txManager = new JpaTransactionManager();
-		txManager.setEntityManagerFactory(entityManagerFactory());
-		return txManager;
-	}
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        final JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory());
+        return txManager;
+    }
 
-	@Bean
-	public HibernateExceptionTranslator hibernateExceptionTranslator() {
-		return new HibernateExceptionTranslator();
-	}
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
+    }
 
 }
